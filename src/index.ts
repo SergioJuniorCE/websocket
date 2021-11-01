2c5b6eeb3526b824aade369ccfd4a732a991f4ff
Sergio Junior Castro Egurrolar <sergiojuniorce@gmail.com>
2021-11-01 01:47:30 -0700

    changed WebSocket from js to ts
    Co-Authored-By: José Alberto Trejo Serrano <38301666+joseatrejos@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-const WebSocket = require('ws');
+import WebSocket from "ws";
 
-const port = process.env.PORT || 8080;
+const port = 8080;
 const wss = new WebSocket.Server({ port });
 
-console.log(`Started websocket on ws://localhost:${port}`)
+console.log(`Started websocket on ws://localhost:${port}`);
 
-wss.on("connection", ws => {
+wss.on("connection", (ws) => {
   console.log("Client connected.");
 
   setInterval(() => {
     ws.send(new Date().toISOString());
   }, 100);
 
-  ws.on("message", message => {
+  ws.on("message", (message: string) => {
     console.log(`Message received: ${message}`);
     ws.send(`Echo: ${message}`);
   });
-})
+});
