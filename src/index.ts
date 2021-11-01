e040a9c11cd8200e9b48b122415922265c808192
Sergio Junior Castro Egurrolar <sergiojuniorce@gmail.com>
2021-11-01 02:47:24 -0700

    changed index to a class

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,4 @@
-import WebSocket, { WebSocketServer } from "ws";
+import { TestWebsocket } from './ws-controller';
 
-const port = 8080;
-const wss = new WebSocketServer({
-  port,
-});
-
-console.log(`Started websocket on ws://localhost:${port}`);
-
-wss.on("connection", (ws: WebSocket) => {
-  console.log("Client connected.");
-
-  setInterval(() => {
-    let date: string = new Date().toISOString();
-    ws.send(JSON.stringify({date}));
-  }, 100);
-
-  ws.on("message", (message: string) => {
-    console.log(`Message received: ${message}`);
-    ws.send(`Echo: ${message}`);
-  });
-});
+let ws = new TestWebsocket(8080);
+ws.start(1);
---
+++ src/ws-controller.ts
@@ -0,0 +1,42 @@
+import WebSocket, { WebSocketServer } from "ws";
+
+export class TestWebsocket {
+  private port: number;
+  private wss: WebSocketServer;
+
+  constructor(port: number) {
+    console.log(`Started websocket on ws://localhost:${port}`);
+    this.port = port;
+    this.wss = new WebSocketServer({ port: this.port });
+  }
+
+  /**
+   * Handles a new websocket connection.
+   */
+  public start(interval?: number): void {
+    this.wss.on("connection", (ws: WebSocket) => {
+      console.log("Client connected.");
+
+      if (interval != undefined && interval > 0) {
+        this.testWebSocket(ws, interval);
+      }
+
+      ws.on("message", (message: string) => {
+        console.log(`Message received: ${message}`);
+        ws.send(`Echo: ${message}`);
+      });
+    });
+  }
+
+  /**
+   * Tests the websocket sending the current date every second.
+   * @param ws Websocket from connection
+   * @param interval Interval in milliseconds
+   */
+  private testWebSocket(ws: WebSocket, interval: number): void {
+    setInterval(() => {
+      let date: string = new Date().toISOString();
+      ws.send(JSON.stringify({ currentTime: date }));
+    }, interval);
+  }
+}
