e6f1a22c83be7c1e05850e88cd203036c6563071
Sergio Junior Castro Egurrolar <sergiojuniorce@gmail.com>
2021-11-01 02:18:13 -0700

    fixed message

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import WebSocket from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 
 const port = 8080;
-const wss = new WebSocket.Server({ port });
+const wss = new WebSocketServer({
+  port,
+});
 
 console.log(`Started websocket on ws://localhost:${port}`);
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws: WebSocket) => {
   console.log("Client connected.");
 
   setInterval(() => {
-    ws.send(new Date().toISOString());
+    let date: string = new Date().toISOString();
+    ws.send(JSON.stringify({date}));
   }, 100);
 
   ws.on("message", (message: string) => {
